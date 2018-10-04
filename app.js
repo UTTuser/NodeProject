0494c109005397b1da57f9c06c176e3cf31671c4
Aparna Rajesh <aparna.rajesh@utt.edu.tt>
2018-10-04 14:43:01 -0400

    my first commit for node app

---
+++ app.js
@@ -0,0 +1,10 @@
+var _ = require('underscore');
+
+// node assumes that 
+//core module
+//file or folder
+//node_modules
+// use functions provided by the modules
+
+var check = _.contains([1,2,3,4],2);
+console.log(check);
